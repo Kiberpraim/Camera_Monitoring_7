f99cf7c46b5f1c790eed795b7d1c18ddeea5f3f3
Kiberpraim <abdykulovalmanbet4@gmail.com>
2023-09-19 19:05:49 +0600

    ᓚᘏᗢ

---
+++ app/src/main/java/com/geeks/camera_monitoring_7/presentation/ui/activity/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.geeks.camera_monitoring_7.presentation.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.geeks.camera_monitoring_7.databinding.ActivityMainBinding
+import com.geeks.camera_monitoring_7.presentation.ui.activity.adapter.ViewPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val tabLayout = binding.tabLayout
+        val viewPager2 = binding.viewPager2
+
+        val adapter = ViewPagerAdapter(supportFragmentManager,lifecycle)
+
+        viewPager2.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager2){tab, position ->
+            when(position){
+                0 -> tab.text = "Камеры"
+                1 -> tab.text = "Комнаты"
+            }
+        }.attach()
+    }
+}
---
+++ app/src/main/java/com/geeks/camera_monitoring_7/presentation/ui/activity/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,22 @@
+package com.geeks.camera_monitoring_7.presentation.ui.activity.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.geeks.camera_monitoring_7.presentation.ui.fragment.cameras.CamerasFragment
+import com.geeks.camera_monitoring_7.presentation.ui.fragment.doors.DoorsFragment
+
+class ViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+    FragmentStateAdapter(fragmentManager, lifecycle) {
+
+    override fun getItemCount(): Int = 2
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> CamerasFragment()
+            1 -> DoorsFragment()
+            else -> CamerasFragment()
+        }
+    }
+}
